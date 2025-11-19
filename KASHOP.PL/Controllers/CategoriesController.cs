d9a028a7e2d22aa37f769a36766e5866595ffaaa
rahaf salhab <rahafsalhab16@gmail.com>
2025-11-19 22:08:47 +0200

    first commit

---
+++ KASHOP.PL/Controllers/CategoriesController.cs
@@ -0,0 +1,19 @@
+﻿using KASHOP.DAL.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KASHOP.PL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public CategoriesController(ApplicationDbContext context) 
+        {
+            this.context = context;
+        }
+        
+    }
+}
